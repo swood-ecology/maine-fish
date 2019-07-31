fe4981d322e9dafea9a395b9bcafc459d9b47584
Stephen Wood <stephenawood@gmail.com>
2019-07-31 08:23:59 -0400

    Minor changes

--- lobstah-DLM.R
+++ lobstah-DLM.R
@@ -6,53 +6,61 @@ library(rjags)
 fish.data <- readRDS("~/Box Sync/Courses/NEFI/Allyn_EcoForecastingProjectData.rds")
 
 # Filter out lobster in Maine
-maine.lobstah <- fish.data %>% 
+maine.lobstah <- fish.data %>%
   select(-Survey, -X) %>%
   filter(CommonName == "AMERICAN LOBSTER") %>%
   filter(Y > 42.874)
 
 # Plot all data
-ggplot(maine.lobstah, aes(y = Biomass, x = Year)) + 
-  geom_point(alpha = 0.5) + 
-  ylim(0,500) +
+ggplot(maine.lobstah, aes(y = Biomass, x = Year)) +
+  geom_point(alpha = 0.5) +
+  ylim(0, 500) +
   theme_bw()
 
 # Take annual average
-mean.maine.lobstah <- aggregate(cbind(Biomass, Depth, SeasonalSST) ~ Year, 
-                                maine.lobstah, mean)
+mean.maine.lobstah <- aggregate(
+  cbind(Biomass, Depth, SeasonalSST) ~ Year,
+  maine.lobstah, mean
+)
 
 # Fit DLM model
 ## fit the model
-ef.out <- ecoforecastR::fit_dlm(model=list(obs="Biomass",fixed="~ 1 + X + Depth + SeasonalSST"),
-                                mean.maine.lobstah)
+ef.out <- ecoforecastR::fit_dlm(
+  model = list(obs = "Biomass", fixed = "~ 1 + X + Depth + SeasonalSST"),
+  mean.maine.lobstah
+)
 
 ## parameter diagnostics
-params <- window(ef.out$params,start=1000) ## remove burn-in
+params <- window(ef.out$params, start = 1000) ## remove burn-in
 summary(params)
 cor(as.matrix(params))
 
 ## confidence interval
 dlm.out <- as.matrix(ef.out$predict)
-dlm.ci <- apply(dlm.out,2,quantile,c(0.025,0.5,0.975))
+dlm.ci <- apply(dlm.out, 2, quantile, c(0.025, 0.5, 0.975))
 plot(mean.maine.lobstah$Year,
-     dlm.ci[2,],type='n')
+  dlm.ci[2, ],
+  type = "n"
+)
 ## adjust x-axis label to be monthly if zoomed
-ecoforecastR::ciEnvelope(mean.maine.lobstah$Year,dlm.ci[1,],dlm.ci[3,],col=ecoforecastR::col.alpha("lightBlue",0.75))
-points(mean.maine.lobstah$Year,mean.maine.lobstah$Biomass,pch="+",cex=0.5)
+ecoforecastR::ciEnvelope(mean.maine.lobstah$Year, dlm.ci[1, ], dlm.ci[3, ], col = ecoforecastR::col.alpha("lightBlue", 0.75))
+points(mean.maine.lobstah$Year, mean.maine.lobstah$Biomass, pch = "+", cex = 0.5)
 
 
 # Take seasonal average
-season.maine.lobstah <- aggregate(cbind(Biomass, Depth, SeasonalSST) ~ Year + Season, 
-                                maine.lobstah, mean)
+season.maine.lobstah <- aggregate(
+  cbind(Biomass, Depth, SeasonalSST) ~ Year + Season,
+  maine.lobstah, mean
+)
 
 # Plot average biomass trend
-ggplot(season.maine.lobstah, aes(x = Year, y = Biomass, facet = Season)) + 
-  geom_line(aes(color = Season)) + 
+ggplot(season.maine.lobstah, aes(x = Year, y = Biomass, facet = Season)) +
+  geom_line(aes(color = Season)) +
   theme_bw()
 
 
 
-lobstah_rand = "
+lobstah_rand <- "
 model{                                                                                                 
                                                                                                        
   #### Priors                                                                                            
@@ -93,23 +101,38 @@ model{
 "
 
 # Define data and parameters
-season.data <- list(y=season.maine.lobstah$Biomass,
-             nrep=unique(season.maine.lobstah$Season),
-             n=length(season.maine.lobstah$Biomass),
-             x_ic=20,
-             tau_ic=0.5,a_obs=1,r_obs=1,a_add=1,r_add=1)
+season.data <- list(
+  y = season.maine.lobstah$Biomass,
+  nrep = unique(season.maine.lobstah$Season),
+  n = length(season.maine.lobstah$Biomass),
+  x_ic = 20,
+  tau_ic = 0.5, a_obs = 1, r_obs = 1, a_add = 1, r_add = 1
+)
 
 # Define initial values
-nchain = 3
+nchain <- 3
 init <- list()
-for(i in 1:nchain){
-  y.samp = sample(mean.maine.lobstah$Biomass,length(mean.maine.lobstah$Biomass),
-                  replace=TRUE)
-  init[[i]] <- list(tau_add=1/var(diff(log(y.samp))),tau_obs=5/var(log(y.samp)))
+for (i in 1:nchain) {
+  y.samp <- sample(mean.maine.lobstah$Biomass, length(mean.maine.lobstah$Biomass),
+    replace = TRUE
+  )
+  init[[i]] <- list(tau_add = 1 / var(diff(log(y.samp))), tau_obs = 5 / var(log(y.samp)))
 }
 
 # Run model
-j.model   <- jags.model (file = textConnection(RandomWalk),
-                         data = data,
-                         inits = init,
-                         n.chains = 3)
+j.model <- jags.model(
+  file = textConnection(RandomWalk),
+  data = data,
+  inits = init,
+  n.chains = 3
+)
+
+
+#########################################################
+
+# Read in lobster data with management groupings
+lobstah.spatial <- readRDS("~/Box Sync/Courses/NEFI/maine-fish/data/LobsterEcoForecastingProjectData.rds")
+
+# Select only needed columns
+lobstah.spatial %>%
+  select(Year:Depth, ZONEID, Long, Lat) -> lobstah.spatial
