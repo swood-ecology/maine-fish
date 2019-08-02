4128db0ccbfa399853cd857683c221bc755590cd
Stephen Wood <stephenawood@gmail.com>
2019-08-02 14:00:08 -0400

    Add model for disaggregated data

--- lobstah-DLM.R
+++ lobstah-DLM.R
@@ -14,7 +14,9 @@ lobstah.data <- lobstah.data %>% select(Year:Depth, ZONEID)
 lobstah.data$logBiomass <- log(lobstah.data$Biomass + 0.000001) 
 
 lobstah <- lobstah.data %>%
-  filter(logBiomass > mean(lobstah.data$logBiomass) - 2*sd(lobstah.data$logBiomass))
+  filter(logBiomass > mean(lobstah.data$logBiomass) - 
+           2*sd(lobstah.data$logBiomass))
+lobstah$numZone <- lobstah$ZONEID %>% as.numeric()
 
 mean.lobstah.data <- aggregate(cbind(logBiomass, Depth, SeasonalSST) ~ Year + ZONEID, 
                                lobstah %>% filter(Season == "SPRING"), 
@@ -111,13 +113,12 @@ model{
 
   #### Random Effect
   for(g in 1:ng){
-    beta_site[g] ~ dnorm(0.2,0.1)
-    alpha_site[g] ~ dnorm(2.0,0.1)
+    beta_site[g] ~ dnorm(0.2, 0.1)
   }
 
   #### Process Model
   for(i in 2:n){
-    mu[i] <- x[i-1] + alpha_site[zoneid[i]] + beta_site[zoneid[i]]*sst[i]
+    mu[i] <- x[i-1] + alpha + beta_site[zoneid[i]]*sst[i]
     x[i] ~ dnorm(mu[i], tau_add)
   }
 
@@ -125,12 +126,13 @@ model{
   x[1] ~ dnorm(x_ic,tau_ic)
   tau_obs ~ dgamma(0.1,0.1)
   tau_add ~ dgamma(0.1,0.1)
+  alpha ~ dnorm(2, 0.1)
 }
 "
 
 h.dlm.data <- list(y = logBiomass, sst = SeasonalSST,
                    n = logBiomass %>% length(), 
-                   ng = ZONEID %>% unique() %>% length(), 
+                   ng = ZONEID %>% unique() %>% length(),
                    zoneid = ZONEID %>% as.numeric(), 
                    x_ic = 2.5, tau_ic = 0.1)
 
@@ -139,14 +141,90 @@ h.dlm.model <- jags.model(data = h.dlm.data,
                           n.chains = 4)
 
 h.dlm.out <- coda.samples (model = h.dlm.model,
-                           variable.names = c("alpha_site","beta_site",
+                           variable.names = c("alpha","beta_site",
                                               "tau_add","tau_obs","x"),
                            n.iter = 10000)
 gelman.plot(h.dlm.out)
 
-burnin = 8000
+burnin = 6000
 hrw.burn <- window(h.dlm.out, burnin)
 summary(hrw.burn)
 
-
 detach(mean.lobstah.data)
+
+
+##################################
+# Run for all data, not averages #
+##################################
+RSDLM.AD = "
+model{
+  #### Data Model
+  for(i in 1:n){
+    y[i] ~ dnorm(x[i],tau_obs)
+  }
+
+  #### Random slope
+  for(g in 1:ng){
+    beta_site[g] ~ dnorm(0.2, 0.1)
+  }
+
+  #### Year-based random walk value
+  for(t in year1:(year1+ny)){
+    x_year[t] ~ dnorm(0.2, 0.1)
+  }
+
+  #### Process Model
+  for(i in 2:n){
+    x[i] ~ dnorm(mu[i], tau_add)  
+    mu[i] <- x_year[year[i-1]] + alpha + beta_site[zoneid[i]]*sst[i]
+  }
+
+  #### Priors
+  x[1] ~ dnorm(x_ic,tau_ic)
+  tau_obs ~ dgamma(0.1,0.1)
+  tau_add ~ dgamma(0.1,0.1)
+  alpha ~ dnorm(2, 0.1)
+}
+"
+
+attach(lobstah)
+h.dlm.data <- list(y = logBiomass, sst = SeasonalSST,
+                   n = logBiomass %>% length(), 
+                   ng = ZONEID %>% unique() %>% length(),
+                   ny = Year %>% unique() %>% length(),
+                   year1 = Year %>% as.numeric() %>% min(),
+                   year = Year %>% as.numeric(),
+                   zoneid = numZone, 
+                   x_ic = 2.5, tau_ic = 0.1)
+
+h.dlm.model <- jags.model(data = h.dlm.data, 
+                          file = textConnection(RSDLM.AD),
+                          n.chains = 4)
+
+h.dlm.out <- coda.samples (model = h.dlm.model,
+                           variable.names = c("alpha","beta_site",
+                                              "tau_add","tau_obs","x_year"),
+                           n.iter = 10000)
+gelman.plot(h.dlm.out)
+
+burnin = 6000
+hrw.burn <- window(h.dlm.out, burnin)
+summary(hrw.burn)
+
+out <- as.matrix(hrw.burn)[,2:8]
+
+hist <- data.frame(out[,1],
+           out[,2],
+           out[,3],
+           out[,4],
+           out[,5],
+           out[,6],
+           out[,7]
+)
+
+ggplot(reshape::melt(hist)) + geom_freqpoly(aes(x = value,
+                                 y = ..density.., colour = variable)) +
+  theme_bw()
+
+detach(lobstah)
+
